d2521fbf2f66ecc988a4ba1d4dd8a19895c95633
Erica Murdock-Waters <ericahpa@gmail.com>
2018-03-04 21:42:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  if array.length < 3
+    array.join(" and ")
+  elsif array[-1].length > 1
+    #array.scan(/pattern/) { |match|  }
+    array.each do | e |
+      if e.count < 1
+        array.join(", ").split(" ").insert(array.length - 1, "and").join(" ")
+      else
 
+      end
+    end
+
+
+  else
+    array.join(", ").split(" ").insert( - 1, "and").join(" ")
+  end
 end
